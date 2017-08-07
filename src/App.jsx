f9318ad1e703c54ef691184e2d6ec6805eba8da8
anonymouslooker <anonymouslooker.me@gmail.com>
2017-08-07 01:41:06 -0400

    Imported redux connect and connections actions and state to props

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,28 @@
 import React from 'react';
+import PropTypes from 'prop-types';
+import { connect } from 'react-redux';
+
 import logo from './logo.svg';
 import './App.css';
 
+import { submitTodo } from './actions/';
+
 import AddTodo from './components/AddTodo/';
 
-const App = () => (
+export const App = props => (
   <div className="App">
     <div className="App-header">
       <img src={logo} className="App-logo" alt="logo" />
       <h2>Welcome to React: Todo in action!</h2>
     </div>
     <div className="todo-container">
-      <AddTodo submitTodo={() => {}} />
+      <AddTodo submitTodo={props.submitTodo} />
     </div>
   </div>
 );
 
-export default App;
+App.propTypes = {
+  submitTodo: PropTypes.func.isRequired,
+};
+
+export default connect(state => state.todoListApp, { submitTodo })(App);
